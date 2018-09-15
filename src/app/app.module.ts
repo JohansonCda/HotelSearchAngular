431f5ad1766248373408f1405d52cc78435d98b9
Johanson <jcv9806@hotmail.com>
2018-09-15 19:55:25 +0000

    Header Componet cambiado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { InicioComponent } from './page/inicio/inicio.component';
+import { QuienessomosComponent } from './page/quienessomos/quienessomos.component';
+import { PruebaComponent } from './compo/prueba/prueba.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent,
+    QuienessomosComponent,
+    PruebaComponent
   ],
   imports: [
     BrowserModule
